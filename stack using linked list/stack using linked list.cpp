55ae34a7ba1f2bf091849a44e385741102a2bb13
sabilafiridas <sabilafirdias@gmail.com>
2024-05-11 10:52:44 +0700

    membuat class stack dengan akses public

--- stack using linked list/stack using linked list.cpp
+++ stack using linked list/stack using linked list.cpp
@@ -5,20 +5,37 @@
 using namespace std;
 
 //node class representing a single node in the linked list
-class node {
+class Node {
 public:
     int data;
-    node* next;
+    Node* next;
 
-    node() {
+    Node() {
         next = NULL;
     }
 };
 
 //stack class
-class stack {
+class Stack {
 private:
-    node* top;      //pointer to the top node of the stack
+    Node* top;      //pointer to the top node of the stack
+
+public:
+    Stack() {
+        top = NULL;     //initialize the stack with a null top pointer
+    }
+
+    //push operation: insert an element onto the top of the stack
+    int push(int value) {
+        Node* newNode = new Node(); //1. allocate memory for the new node
+        newNode->data = value;  //2. assign value
+        newNode->next = top;  //3. set the next pointer of the new node to the current top node
+        top = newNode;      //4. update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    
 };
 
 int main()
