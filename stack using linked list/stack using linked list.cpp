72b4315cff3bfdca7ab2be9422d1ce05f24a2444
sabilafiridas <sabilafirdias@gmail.com>
2024-05-11 10:42:28 +0700

    Add project files.

---
+++ stack using linked list/stack using linked list.cpp
@@ -0,0 +1,13 @@
+// stack using linked list.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+//node class representing a single node in the linked list
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
