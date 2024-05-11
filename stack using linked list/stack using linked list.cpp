b0e706aa97878844abe8cd386f187f872f020615
sabilafiridas <sabilafirdias@gmail.com>
2024-05-11 10:58:55 +0700

    membuat operasi peek

--- stack using linked list/stack using linked list.cpp
+++ stack using linked list/stack using linked list.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-//node class representing a single node in the linked list
+// node class representing a single node in the linked list
 class Node {
 public:
     int data;
@@ -15,22 +15,22 @@ public:
     }
 };
 
-//stack class
+// stack class
 class Stack {
 private:
-    Node* top;      //pointer to the top node of the stack
+    Node* top;      // pointer to the top node of the stack
 
 public:
     Stack() {
-        top = NULL;     //initialize the stack with a null top pointer
+        top = NULL;     // initialize the stack with a null top pointer
     }
 
-    //push operation: insert an element onto the top of the stack
+    // push operation: insert an element onto the top of the stack
     int push(int value) {
-        Node* newNode = new Node(); //1. allocate memory for the new node
-        newNode->data = value;  //2. assign value
-        newNode->next = top;  //3. set the next pointer of the new node to the current top node
-        top = newNode;      //4. update the top pointer to the new node
+        Node* newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value;  // 2. assign value
+        newNode->next = top;  // 3. set the next pointer of the new node to the current top node
+        top = newNode;      // 4. update the top pointer to the new node
         cout << "Push value: " << value << endl;
         return value;
     }
@@ -40,7 +40,13 @@ public:
         if (isEmpty()) {
             cout << "Stack is empty." << endl;
         }
+        cout < "Popped value: " << top->data << endl;
+        top = top->next;        // update the top pointer to the next node
+    }
 
+    // peek/top operation: retrieve the value of the topmost element without removing it
+    void peek() {
+       
     }
 };
 
