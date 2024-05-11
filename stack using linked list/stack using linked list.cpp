146569d638dc33eadaca8081a706bdbddd759c38
sabilafiridas <sabilafirdias@gmail.com>
2024-05-11 11:13:13 +0700

    membuat pilihan case 1

--- stack using linked list/stack using linked list.cpp
+++ stack using linked list/stack using linked list.cpp
@@ -79,5 +79,14 @@ int main()
         cout << "4. Exit\n";
         cout << "Enter your choice\n";
         cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);  // push the entered value onto the stack
+            break;
+        
+        }
     }
 }
